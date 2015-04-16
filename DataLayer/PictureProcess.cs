5e2abf45b2091e0b2112c72a0c944c6f6c7afd56
Chen Chen <bzsmz1990@gmail.com>
2015-04-16 12:58:11 -0400

    Add PictureProcess class

---
+++ DataLayer/PictureProcess.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class PictureProcess
+    {
+        //Valid file type
+        //Since different companies has different file extension for their RAW file
+        //We only conside Nikon(.nef), Canon(.cr2), SONY(.arw), PENTAX(.pef) and LEICA(.dng)
+        public enum ValidFileType
+        {
+            jpg, bmp, cr2, nef, arw, pef, dng
+        }
+
+        //Extract file extension
+        public static string GetFileExtends(string filename)
+        {
+            string ext = null;
+            if (filename.IndexOf('.') > 0)
+            {
+                string[] fs = filename.Split('.');
+                ext = fs[fs.Length - 1];
+            }
+            return ext;
+        }
+
+        //Check whether file's extension is valid
+        public static bool CheckFileExtends(string fileExtends)
+        {
+            bool status = false;
+            fileExtends = fileExtends.ToLower();
+            string[] fe = Enum.GetNames(typeof(ValidFileType));
+            for (int i = 0; i < fe.Length; i++)
+            {
+                if (fe[i].ToLower() == fileExtends)
+                {
+                    status = true;
+                    break;
+                }
+            }
+            return status;
+        }
+    }
+}
---
+++ PhotoProject/Controllers/UploadPicController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoProject.Controllers
+{
+    public class UploadPicController : Controller
+    {
+        // GET: UploadPic
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
