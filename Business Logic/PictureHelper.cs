4e24c60351773ac1fa31fc6dbcf4e6e82e4770e9
Agatha Oliveira <alo270@nyu.edu>
2015-04-17 02:42:49 -0400

    Adding business logic layer

---
+++ Business Logic/PictureHelper.cs
@@ -0,0 +1,74 @@
+﻿using DataLayer;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic
+{
+    public class PictureHelper
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ICollection<Picture> GetPicturesOrderedByMostPurchased()
+        {
+            return db.Pictures.OrderByDescending(p => p.SaleTransactions.Count).ToList();
+        }
+
+        public ICollection<Picture> GetPicturesOrderedByMostRecent()
+        {
+            return db.Pictures.OrderByDescending(p => p.UploadTime).ToList();
+        }
+
+        public ICollection<Picture> GetPicturesWhereTitleHasWord(string searchString)
+        {
+            if (String.IsNullOrEmpty(searchString) || String.IsNullOrWhiteSpace(searchString))
+            {
+                return db.Pictures.ToList();
+            }
+
+            return db.Pictures.Where(p => p.Title.Contains(searchString)).ToList();
+        }
+
+        public ICollection<Picture> GetPicturesWhereDescriptionHasWord(string searchString)
+        {
+            if (String.IsNullOrEmpty(searchString) || String.IsNullOrWhiteSpace(searchString))
+            {
+                return db.Pictures.ToList();
+            }
+
+            return db.Pictures.Where(p => p.Description.Contains(searchString)).ToList();
+        }
+
+        public ICollection<Picture> GetPicturesWhereTagHasWord(string searchString)
+        {
+            if (String.IsNullOrEmpty(searchString) || String.IsNullOrWhiteSpace(searchString))
+            {
+                return db.Pictures.ToList();
+            }
+
+
+            //TODO: can writ e aquery to reutnr based on relationships?
+            return null;
+        }
+
+        public void LikePicture (Picture picture, UserInfo user)
+        {
+            if (!picture.LikedBy.Contains(user))
+            {
+                picture.NumberOfLikes++;
+                picture.LikedBy.Add(user);
+                //TODO: ALSO ADD CREDIT TO OWNER
+            }
+        }
+
+        public void ReportPicture(Picture picture, UserInfo user)
+        {
+            picture.HasBeenReported = true;
+            //TODO: SEND NOTIFICATION
+        }
+        
+    }
+}
