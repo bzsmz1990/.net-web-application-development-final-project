88ed8a9c1441d2c8c3903f4016e1ca7145eb9831
DiegoRincon <drs414@nyu.edu>
2015-04-24 11:05:32 -0400

    Working on cart controller

--- DataLayer/Picture.cs
+++ DataLayer/Picture.cs
@@ -53,7 +53,7 @@ namespace DataLayer
         public bool HasBeenReported { get; set; }
 
         [Required]
-        public byte[] OriginalImg { get; set; }  //The image as an url
+        public byte[] OriginalImg { get; set; }
 
         [Required]
         public byte[] CompressImg { get; set; }
