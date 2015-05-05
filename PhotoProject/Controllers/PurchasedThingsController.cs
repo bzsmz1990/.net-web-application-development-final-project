a8ba21c8d7fd0f16f8cd3ff5a7f4d6ed01225244
Chen Chen <bzsmz1990@gmail.com>
2015-05-05 16:17:09 -0400

    Create controller to show original pictures user bought

---
+++ PhotoProject/Controllers/PurchasedThingsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+using Business_Logic;
+
+namespace PhotoProject.Controllers
+{
+    public class PurchasedThingsController : Controller
+    {
+        private static PictureHelper picHelp = new PictureHelper(AlbumDetailsController.db);
+        // GET: PurchasedThings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OriginalImg(int id)
+        {
+            Picture pic = new Picture();
+            pic = picHelp.GetSpecificPicture(id);
+            return View(pic);
+        }
+    }
+}
